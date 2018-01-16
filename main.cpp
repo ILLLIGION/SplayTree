5c2273abd74c4190d6b890058c1614afcd08a937
ILLLIGION <dazuk07@yandex.ru>
2018-01-16 14:36:20 +0300

    no test

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "library.h"
+
+int main ()
+{
+    SplayTree<int> Tree1{3,2,1,0,65,34,243,15,53,32,412,342};
+    std::cout << "nu ono nakonez-to sobralos" << std::endl;
+
+
+    return 0;
+}
