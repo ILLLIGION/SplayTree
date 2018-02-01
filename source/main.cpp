10596103537153ed0d83fdbd875fa0d24da4c492
ILLLIGION <dazuk07@yandex.ru>
2018-02-01 15:40:25 +0300

    print added

--- source/main.cpp
+++ source/main.cpp
@@ -6,14 +6,17 @@ int main ()
     Tree1.insert(5,2);
     Tree1.insert(8,4);
     Tree1.insert(10,5);
+    Tree1.insert(3,8);
+    Tree1.insert(6,1234);
+    Tree1.insert(48,51);
+    Tree1.insert(7,14);
+    Tree1.insert(9,30);
+    Tree1.insert(11,534);
     //Tree1.remove(5);
-    Tree1.remove(0);
+    //Tree1.remove(0);
     Tree1.search(5);
-    Tree1.print(std::cout, Tree1.root_);
+    Tree1.print(std::cout, Tree1.root_, 0);
     std::cout << "nu ono nakonez-to sobralos" << std::endl;
-    std::cout << *Tree1.max();
-    std::cout << std::endl;
-    std::cout << *Tree1.min();
 
 
     return 0;
